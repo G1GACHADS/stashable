bbd4aefda708cacef4d4de06e74d9e575e8498a6
Hansen Andersen <hansen.sutikno@binus.ac.id>
2022-09-04 00:02:17 +0700

    search bar component in main page

--- src/screens/main/index.jsx
+++ src/screens/main/index.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import { Image, ScrollView, StatusBar, View } from 'react-native'
 import { useTheme } from 'styled-components/native'
 
 import routes from '../../constants/routes'
 
+import Search from '../../components/search'
 import Button from '../../components/button'
 import Container from '../../components/container'
-import Text from '../../components/text'
+
+import IconSearch from '../../components/icons/icon-search'
 
 import MoreWarehouse from './more-warehouse'
 import Featured from './main-featured'
@@ -15,32 +18,47 @@ import IconElectricCategory from '../../components/icons/icon-electric-category'
 
 export function MainScreen({ navigation }) {
   const theme = useTheme()
+  const [search, setForm] = useState({
+    // email: '',
+    // password: '',
+  })
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
       <ScrollView>
-        <Container
-          backgroundColor={theme.colors.grey2}
-          style={{ alignItems: 'center', justifyContent: 'center' }}
-        >
-          <Text
-            color={theme.colors.grey3}
-            weight={theme.typography.weight.medium}
-            size={theme.typography.tall.md}
-          >
-            Search Bar
-          </Text>
-        </Container>
         <Container>
-          {/* Banner */}
-          <Image
-            source={require('../../assets/images/stashable-sale-banner.png')}
-            style={{
-              width: null,
-              height: 120,
-            }}
-          ></Image>
-        </Container>
+
+        <View 
+          style={{
+            display:'flex',
+            flexDirection:'row',
+            borderRadius: 44,
+            paddingHorizontal:40,
+            paddingVertical:12,
+            justifyContent:'center',
+            alignItems:'center',
+            gap: 8,
+            backgroundColor:theme.colors.white2,
+            marginVertical: 38,
+            
+          }}
+          >
+          <IconSearch />
+          <Search 
+          placeholder="Search for a place..."
+          />
+        </View>
+        {/* Banner */}
+        <Image
+          source={require('../../assets/images/stashable-sale-banner.png')}
+          style={{
+            width: null,
+            height: 120,
+            borderRadius:10,
+          }}
+        />
+        {/* End Banner */}
         <Place navigation={navigation} />
         <Featured navigation={navigation} />
         <MoreWarehouse navigation={navigation} />
@@ -50,8 +68,9 @@ export function MainScreen({ navigation }) {
             sm
             title="Get Started"
             onPress={() => navigation.navigate(routes.loginRoute)}
-          />
+            />
         </View>
+        </Container>
       </ScrollView>
     </>
   )
